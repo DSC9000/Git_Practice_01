26569af237c7c5dfb98352782862b38e5e537f52
Daniel S <devolution.sdc@gmail.com>
2021-10-03 15:51:56 -0500

    again one more kitty

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -3,5 +3,7 @@ package tests;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello kitty");
+
+        System.out.println("One more kitty ");
     }
 }
